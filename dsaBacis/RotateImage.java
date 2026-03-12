655a46f5d87bd561a605b6d4beec4722e0492b1e
ADITYA SINGH <sadityasingh7990@gmail.com>
2026-03-12 22:18:22 +0530

    Initial commit

--- dsaBacis/RotateImage.java
+++ dsaBacis/RotateImage.java
@@ -1,6 +1,20 @@
 import java.util.*;
 public class RotateImage {
     public static void main(String[] args) {
-
+        int nums[]={23,45,1,2,8,19,-3,16,-11,28};
+        int target=19;
+        boolean ans = linearSearch(nums,target);
+        System.out.println(ans);
+        }
+        static boolean linearSearch(int[] arr, int target) {
+            if (arr.length == 0) {
+                return false;
+            }
+            for (int element : arr) {
+                if (element == target) {
+                    return true;
+                }
+            }
+            return false;
         }
     }
