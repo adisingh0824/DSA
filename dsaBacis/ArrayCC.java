71c6adcb1ad2472081df7c8a35033551e04e88e1
adisingh0824 <sadityasingh7990@gmail.com>
2026-03-19 01:50:48 +0530

    Initial commit

---
+++ dsaBacis/ArrayCC.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class ArrayCC {
+//    public static void update(int marks[]){
+//        for(int i=0; i<marks.length; i++){
+//            marks[i]=marks[i] + 1;
+//        }
+//    }
+    public static void main(String args[]){
+//        int marks[] =new int[50];
+//        int number[] = {1,2,3};
+//        int moreNumbers [] = {4,5,6};
+//        String fruits[] = {"apple", "mango", "orange"};
+//        int mark[]= new int[1000];
+//        Scanner sc=new Scanner(System.in);
+//        mark[0]=sc.nextInt();
+//        mark[1]=sc.nextInt();
+//        mark[2]=sc.nextInt();
+//        System.out.println("phy : "+mark[0]);
+//        System.out.println("maths : "+mark[1]);
+//        System.out.println("chem : "+mark[2]);
+//        System.out.println("length of array = "+ mark.length);
+//        int marks[] = {97, 98, 99};
+//        //int nonChangeable=5;
+//        update(marks);
+//        for(int i =0; i < marks.length; i++ ){
+//            System.out.println(marks[i] + " ");
+//        }
+//        System.out.println();
+    }
+}
---
+++ dsaBacis/linerasearch1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class linerasearch1 {
+    public static int lineraSearch(int numbers[], int key){
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main (String args[]){
+        int numbers[]={2,4,6,8,10,12,14,16};
+        int key=16;
+        int index=lineraSearch(numbers,key);
+        if(index == -1){
+            System.out.println("Not found");
+        }else{
+            System.out.println("Key is at index : "+ index );
+        }
+    }
+}
