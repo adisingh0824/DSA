06c1be1908a5c7c3fbe879938472092c5e9ed0ad
adisingh0824 <sadityasingh7990@gmail.com>
2026-03-29 23:33:50 +0530

    Initial commit

--- dsaBacis/linear.java
+++ dsaBacis/linear.java
@@ -9,6 +9,7 @@ public class linear {
     }
     //search in the array: return the index if item found
     // otherwise if item not found return -1
+    ///
     static int linearSearch(int[] arr,int target){
         if(arr.length==0){
             return -1;
