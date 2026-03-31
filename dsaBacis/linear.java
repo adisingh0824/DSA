f1f10ffc4fd5a9ecc987b4c50a3b332e6f75131a
adisingh0824 <sadityasingh7990@gmail.com>
2026-03-31 15:21:03 +0530

    Initial commit

--- dsaBacis/linear.java
+++ dsaBacis/linear.java
@@ -5,11 +5,7 @@ public class linear {
         int target =-50;
         int ans = linearSearch(nums,target);
         System.out.println(ans);
-
     }
-    //search in the array: return the index if item found
-    // otherwise if item not found return -1
-    ///
     static int linearSearch(int[] arr,int target){
         if(arr.length==0){
             return -1;
@@ -21,8 +17,6 @@ public class linear {
                 return index;
             }
         }
-        // this line  will execute if none of the return statement above have executed
-        //hence the target not found
         return -1;
     }
 }
