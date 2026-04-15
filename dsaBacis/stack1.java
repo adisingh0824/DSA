ccea1e366be784d29eefa689fc84e28b7cc9a2db
adisingh0824 <sadityasingh7990@gmail.com>
2026-04-15 15:06:01 +0530

    Initial commit

--- dsaBacis/stack1.java
+++ dsaBacis/stack1.java
@@ -9,9 +9,12 @@ public class stack1    {
             list.add(data);
 
         }
-        public static int pop(){
-            int top=list.get(list.size()  - 1);
-            list.remove(list.size()-1);
+        public static int pop() {
+            if(isEmpty()){
+                return -1;
+            }
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
             return top;
         }
         public  static int peek(){
